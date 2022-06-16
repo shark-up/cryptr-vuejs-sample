5069ac4b6a09fbfbf84b4bab01ed45c5baf40a5b
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-06-16 17:25:18 +0200

    quick project cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Cryptr logo" src="https://www.cryptr.co/" />
+    <HelloWorld msg="Welcome to Cryptr Vue Sample App" />
   </div>
 </template>
 
